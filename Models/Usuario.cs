c3b94a60a18d89fa3ff8e561a006518f97222bbf
Camille <christelihcamille@hotmail.com>
2025-10-27 20:38:49 -0300

    teste

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SQLite;
 
 namespace projetoHope.Models
 {
     public class Usuario
     {
-        public string Nome { get; set; }
-        public string Email { get; set; }
-        public string Senha { get; set; }
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Username { get; set; } 
+        public string Password { get; set; } 
     }
 }
