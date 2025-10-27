e1b27bfd1b6452671704f301ec110c444db69910
Camille <christelihcamille@hotmail.com>
2025-10-27 20:48:57 -0300

    teste

---
+++ Models/Selecionado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace projetoHope.Models
+{
+    public class Selecionado
+    {
+        [PrimaryKey, AutoIncrement] public int Id { get; set; }
+        [Indexed(Unique = true)] public int UserId { get; set; } // 1 seleção por usuário
+        public int ServiceId { get; set; }
+        public DateTime SelectedAt { get; set; } = DateTime.UtcNow;
+    }
+}
